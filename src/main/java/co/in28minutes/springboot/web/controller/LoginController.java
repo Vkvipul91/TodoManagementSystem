86010fa4de19c683461278e61c64dcdc9c1c5a30
= <=>
2020-02-19 16:28:58 +0530

    step04 get query parameter and sending it in model to view

--- src/main/java/co/in28minutes/springboot/web/controller/LoginController.java
+++ src/main/java/co/in28minutes/springboot/web/controller/LoginController.java
@@ -1,7 +1,9 @@
 package co.in28minutes.springboot.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,8 +11,10 @@ public class LoginController {
 
     @RequestMapping("/login")
     //@ResponseBody
-    public String loginMessage(){
+    //public String loginMessage(){
+    public String loginMessage(@RequestParam String name, ModelMap model){
         //return "Hello World";
+        model.put("name", name);
         return "login";
     }
 }
