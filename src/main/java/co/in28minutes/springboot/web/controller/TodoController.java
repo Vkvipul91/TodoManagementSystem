3f491965b19afb40ca97bafd78e5ea1b07c8e071
= <=>
2020-02-21 16:29:10 +0530

    working app without spring secuirty i.e old login page

--- src/main/java/co/in28minutes/springboot/web/controller/TodoController.java
+++ src/main/java/co/in28minutes/springboot/web/controller/TodoController.java
@@ -4,25 +4,31 @@ import co.in28minutes.springboot.web.model.Todo;
 import co.in28minutes.springboot.web.service.LoginService;
 import co.in28minutes.springboot.web.service.TodoService;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @SessionAttributes("name")
 public class TodoController {
     @Autowired
     TodoService todoService ;
+
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(
+				dateFormat, false));
+	}
 
     @RequestMapping(value ="/list-todos", method = RequestMethod.GET)
     public String showTodoList(ModelMap model){
@@ -57,7 +63,7 @@ public class TodoController {
 			return "todo";
 		}
 		
-		todoService.addTodo((String) model.get("name"), todo.getDesc(), new Date(),
+		todoService.addTodo((String) model.get("name"), todo.getDesc(), todo.getTargetDate(),
 				false);
 		return "redirect:/list-todos";
 	}
@@ -78,16 +84,12 @@ public class TodoController {
 
 	@RequestMapping(value = "/update-todo", method = RequestMethod.POST)
 	public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
-
 		if (result.hasErrors()) {
 			return "todo";
 		}
 		
 		todo.setUser((String) model.get("name"));
-		todo.setTargetDate(new Date());
-		
 		todoService.updateTodo(todo);
-
 		return "redirect:/list-todos";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -1,8 +1,4 @@
-<html>
-<head>
-<title>Yahoo!!</title>
-</head>
-<body>
+<%@ include file="common/header.jspf" %>
+<%@ include file="common/navigation.jspf" %>
 Welcome ${name} !! <a href="/list-todos">Click here</a> to manage your todos
-</body>
-</html>
+<%@ include file="common/footer.jspf" %>
